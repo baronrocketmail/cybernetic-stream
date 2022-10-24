3d2f4dc081cd1cc9e245547e1b79eac15b5796d7
freddie <freddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-24 15:17:00 -0700

    initial

--- pages/contexts/gridContexts.js
+++
@@ -1,4 +0,0 @@
-import {createContext} from "react";
-
-export const PrimaryGridRowsContext = createContext([])
-export const PrimaryGridColumnsContext = createContext([])
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,6 @@ import {fetchAllUnits, uploadStateChange} from "./api/DataFetching";
 import React, {createContext, useContext, useState} from "react";
 import {Box, ThemeProvider, createTheme, colors} from "@mui/material";
 import {objectToRows,objectToColumns,getPaymentsDataset, objectToColumnsSecondary} from "./api/dataTransformations"
-import PrimaryGrid from "./PrimaryGrid";
-import SecondaryGrid from "./SecondaryGrid";
-import {PrimaryGridRowsContext, PrimaryGridColumnsContext} from "./contexts/gridContexts";
 
 export async function getStaticProps(){
     let data = await fetchAllUnits()
@@ -13,12 +10,6 @@ export async function getStaticProps(){
         props: {data},
         revalidate: 1
     }
-}
-
-
-async function handleStateChange(gridState ) {
-    console.log("UPDATEEE")
-    // uploadStateChange(JSON.stringify(gridState.columns))
 }
 
 const theme = createTheme({
@@ -29,25 +20,98 @@ const theme = createTheme({
     }
 })
 
+const PrimaryGridColumnsContext = createContext({})
+const PrimaryGridRowsContext = createContext({})
+const SecondaryGridColumnsContext = createContext({})
+const SecondaryGridRowsContext = createContext({})
+const PaymentsDatasetContext = createContext({})
+
 export default function Home(props) {
-    const apiRef = useGridApiRef();
     const [data, setData] = useState(props.data)
     const [paymentsDataset, setPaymentsDataset] = useState(getPaymentsDataset(data))
-    const [primaryGridRows, setPrimaryGridRows] = useState(objectToRows(data))
     const [primaryGridColumns, setPrimaryGridColumns] = useState(objectToColumns(data))
-    const [secondaryGridRows, setSecondaryGridRows] = useState([])
+    const [primaryGridRows, setPrimaryGridRows] = useState(objectToRows(data))
     const [secondaryGridColumns, setSecondaryGridColumns] = useState(objectToColumnsSecondary(data))
+    const [secondaryGridRows, setSecondaryGridRows] = useState([])
 
     return (
         <>
             <ThemeProvider theme={theme}>
-            <PrimaryGridColumnsContext.Provider value={{primaryGridColumns, setPrimaryGridColumns}}>
-            <PrimaryGridRowsContext.Provider value={{primaryGridRows, setPrimaryGridRows}}>
+                <PrimaryGridColumnsContext.Provider value = {{primaryGridColumns, setPrimaryGridColumns}}>
+                <PrimaryGridRowsContext.Provider value = {{primaryGridRows, setPrimaryGridRows}}>
+                <SecondaryGridColumnsContext.Provider value = {{secondaryGridColumns, setSecondaryGridColumns}}>
+                <SecondaryGridRowsContext.Provider value = {{secondaryGridRows, setSecondaryGridRows}}>
+                <PaymentsDatasetContext.Provider value = {{paymentsDataset, setPaymentsDataset}}>
                 <PrimaryGrid/>
-                {/*<SecondaryGrid/>*/}
-            </PrimaryGridRowsContext.Provider>
-            </PrimaryGridColumnsContext.Provider>
+                <SecondaryGrid/>
+                </PaymentsDatasetContext.Provider>
+                </SecondaryGridRowsContext.Provider>
+                </SecondaryGridColumnsContext.Provider>
+                </PrimaryGridRowsContext.Provider>
+                </PrimaryGridColumnsContext.Provider>
             </ThemeProvider>
         </>
+    )
+}
+
+function SecondaryGrid(props){
+
+    const columnsObj = useContext(SecondaryGridColumnsContext)
+    const rowsObj = useContext(SecondaryGridRowsContext)
+
+    return(
+        <Box sx={{height: "100vh", width: "100%"}}>
+            <DataGridPremium
+                components={{Toolbar: GridToolbar}}
+                rowReordering
+                experimentalFeatures={{aggregation: true}}
+                checkboxSelection
+                columns={columnsObj.secondaryGridColumns}
+                rows={rowsObj.secondaryGridRows}/>
+        </Box>
+    )
+}
+
+
+
+
+function PrimaryGrid(){
+
+    const columnsObj = useContext(PrimaryGridColumnsContext)
+    const rowsObj = useContext(PrimaryGridRowsContext)
+    const paymentsDatasetObj = useContext(PaymentsDatasetContext)
+    const secondaryGridRowsObj = useContext(SecondaryGridRowsContext)
+
+    function handlePrimaryGridSelectionModelChange(selectionModel) {
+        let array = []
+        for (let propertyName in selectionModel) {
+            let datasetKey = selectionModel[propertyName]
+            array.push(... paymentsDatasetObj.paymentsDataset[datasetKey])
+        }
+        secondaryGridRowsObj.setSecondaryGridRows(array)
+    }
+
+    function getPrimaryGridDetailPanelContent(props) {
+        return(
+            <>
+                <h1>super estatic, like the matrix</h1>
+            </>
+        )
+    }
+
+    return (
+        <Box sx={{height: "100vh", width: "100%" , border: "0px solid black"}}>
+            <DataGridPremium
+                rowReordering
+                components={{Toolbar: GridToolbar}}
+                getDetailPanelContent={getPrimaryGridDetailPanelContent}
+                getDetailPanelHeight={()=> "auto"}
+                experimentalFeatures={{aggregation: true, newEditingApi: true}}
+                checkboxSelection
+                onSelectionModelChange={(selectionModel) => handlePrimaryGridSelectionModelChange(selectionModel)}
+                columns={columnsObj.primaryGridColumns}
+                rows={rowsObj.primaryGridRows}
+            />
+        </Box>
     )
 }
