29b16a7144ba7ebd6507aed2196dc4d0125f4339
freddie <freddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-23 23:57:33 -0700

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
 import {DataGridPremium, GridToolbar} from "@mui/x-data-grid-premium";
 import {fetchAllUnits} from "./api/DataFetching";
 import {useState} from "react";
-import {Box} from "@mui/material";
+import {Box, ThemeProvider, createTheme, colors} from "@mui/material";
 import {objectToRows,objectToColumns,getPaymentsDataset, objectToColumnsSecondary } from "./api/dataTransformations"
 
+const theme = createTheme({
+    palette: {
+
+        primary:{
+            main: colors.orange[900],
+
+        }
+    }
+})
+const theme2 = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#3f51b5',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+        text: {
+            primary: 'rgba(255,255,255,0.87)',
+            secondary: 'rgba(255,255,255,0.54)',
+            disabled: 'rgba(140,140,140,0.38)',
+        },
+        background: {
+            default: '#000000',
+            paper: '#000000',
+        },
+    },
+})
 
 export async function getStaticProps(){
     let data = await fetchAllUnits()
@@ -12,6 +41,7 @@ export async function getStaticProps(){
         revalidate: 1
     }
 }
+
 
 function getPrimaryGridDetailPanelContent(props) {
     return(
@@ -41,6 +71,8 @@ export default function Home(props) {
 
     return (
         <>
+            <ThemeProvider theme={theme}>
+
             <Box sx={{height: "100vh", width: "100%" , border: "0"}}>
                 <DataGridPremium
                     rowReordering
@@ -63,6 +95,8 @@ export default function Home(props) {
                     columns={secondaryGridColumns}
                     rows={secondaryGridRowsActive}/>
             </Box>
-        </>
+                </ThemeProvider>
+
+                </>
     )
 }
